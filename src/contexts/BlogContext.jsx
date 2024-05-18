7356330adfdf52221cc3363da2cbe5b023586fa5
StructuredTrading <samowen1992@hotmail.com>
2024-05-18 10:02:23 +1000

    feat: Work in progress of moving journal state and editing journal state to global state

--- src/contexts/BlogContext.jsx
+++ src/contexts/BlogContext.jsx
@@ -1,39 +1,55 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useLocalStorage } from "react-use";
+
 
 let defaultJournalData = [
-    {
-        id: 0,
-        title: "Default journal post",
-        content: "Welcome to the website!",
-        author: "Sam",
-        date: Date.now()
-    }
+	{
+		id: 0,
+		title:"Default journal post",
+		content: "Welcome to the website!",
+		author: "Sam",
+		date: Date.now()
+	}
 ];
 
-// This is only the data
+// This is only the data 
 export const JournalDataContext = createContext(null);
-// This is the function to update the data
-export const JournaDispatchContext = createContext(null);
+// This is the function to update the data 
+export const JournalDispatchContext = createContext(null);
 
-export function useJournalDate() {
-    console.log("Journal data is being accessed");
-    return useContext(JournalDataContext);
+// Hook to read the global data 
+export function useJournalData(){
+	console.log("Journal data is being accessed");
+	return useContext(JournalDataContext);
 }
 
-// Hook to call the function that edits the global data
-export function useJournalDispatch() {
-    return useContext(JournaDispatchContext);
+// Hook to call the function that edits the global data 
+export function useJournalDispatch(){
+	return useContext(JournalDispatchContext);
 }
 
-export function BlogProvider(props) {
 
-    let [exampleState, setExampleState] = useState("Hello from global level!");
+export function BlogProvider({children}) {
 
-    return (
-        <JournalDataContext.Provider value={exampleState}>
-            <JournaDispatchContext.Provider value={setExampleState}>
-                {props.children}
-            </JournaDispatchContext.Provider>
-        </JournalDataContext.Provider>
-    )
+	let [journalEntries, setJournalEntries] = useState([]);
+  	let [storedEntries, setStoredEntries] = useLocalStorage("journalEntries", []);
+
+	let [exampleState, setExampleState] = useState("Hello from global level!");
+
+	useEffect(() => {
+		setJournalEntries(storedEntries);
+	
+		// return (() => {
+		//   setStoredEntries(journalEntries);
+		// });
+	  // eslint-disable-next-line react-hooks/exhaustive-deps
+	  }, []);
+
+	return (
+		<JournalDataContext.Provider value={journalEntries} >
+			<JournalDispatchContext.Provider value={setJournalEntries}>
+				{children}
+			</JournalDispatchContext.Provider>
+		</JournalDataContext.Provider>
+	)
 }
